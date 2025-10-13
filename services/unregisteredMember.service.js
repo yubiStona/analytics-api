46cc61a913b4febc12d13bdce69628af9ee688f0
Jeevan Joshi <joshijeewon18@gmail.com>
2025-10-13 17:23:01 +0545

    feat : fetched distinct emails for regsitered and unregistered members

--- services/unregisteredMember.service.js
+++ services/unregisteredMember.service.js
@@ -2,8 +2,8 @@ const {pool, pool2}=require('../config/db');
 
 const getUnregisteredMembersService = async()=>{
   try{
-    const [reg] = await pool.query(`SELECT cemail FROM health_company1.userinfo WHERE cemail IN (SELECT email FROM ssoapp.sso_users where user_type = 'M')`);
-    const [unreg] = await pool.query(`SELECT cemail FROM health_company1.userinfo WHERE cemail NOT IN (SELECT email FROM ssoapp.sso_users where user_type = 'M')`);
+    const [reg] = await pool.query(`SELECT DISTINCT cemail FROM health_company1.userinfo WHERE cemail IN (SELECT email FROM ssoapp.sso_users where user_type = 'M')`);
+    const [unreg] = await pool.query(`SELECT DISTINCT cemail FROM health_company1.userinfo WHERE cemail NOT IN (SELECT email FROM ssoapp.sso_users where user_type = 'M')`);
     return {unreg:unreg, reg:reg, unreg_counts:unreg.length, reg_counts:reg.length};
   } catch (err) {
     console.error(err);
