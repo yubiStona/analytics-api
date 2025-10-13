2099fa6e942fa8b2d835e5f5bc0a1f0cc6ac0acf
Jeevan Joshi <joshijeewon18@gmail.com>
2025-10-13 17:18:28 +0545

    feat : corrected unregisteredMembers API and getNonlogggedInAgent API

--- services/getNonLoggedInAgents.service.js
+++ services/getNonLoggedInAgents.service.js
@@ -3,9 +3,9 @@ const { pool, pool2 } = require("../config/db");
 const getNonLoggedInAgents = async () => {
   try {
     const [rows] = await pool2.query(
-      "select distinct ai.agent_id from agent_info ai join agent_users au on ai.agent_id=au.agent_id where ai.agent_id not in ( select user_id from user_activity_details)"
+      "select distinct ai.agent_id from agent_info ai join agent_users au on ai.agent_id=au.agent_id where ai.agent_status = 'A' AND ai.agent_id not in ( select user_id from user_activity_details)"
     );
-    const [rows2] = await pool2.query(`select distinct ai.agent_id from agent_info ai join agent_users au on ai.agent_id=au.agent_id where ai.agent_id  in ( select user_id from user_activity_details)`);
+    const [rows2] = await pool2.query(`select distinct ai.agent_id from agent_info ai join agent_users au on ai.agent_id=au.agent_id where ai.agent_status = 'A' AND ai.agent_id  in ( select user_id from user_activity_details)`);
     return {nonLoggedin_agents:rows, loggedin_agents:rows2, nonLoggedin_counts:rows.length, loggedin_counts:rows2.length};
   } catch (err) {
     console.error(err);
--- services/unregisteredMember.service.js
+++ services/unregisteredMember.service.js
@@ -2,8 +2,8 @@ const {pool, pool2}=require('../config/db');
 
 const getUnregisteredMembersService = async()=>{
   try{
-    const [reg] = await pool.query(`SELECT distinct userid FROM health_company1.userinfo WHERE userid IN (SELECT distinct id FROM ssoapp.sso_users)`);
-    const [unreg] = await pool.query(`SELECT distinct userid FROM health_company1.userinfo WHERE userid NOT IN (SELECT distinct id FROM ssoapp.sso_users)`);
+    const [reg] = await pool.query(`SELECT cemail FROM health_company1.userinfo WHERE cemail IN (SELECT email FROM ssoapp.sso_users where user_type = 'M')`);
+    const [unreg] = await pool.query(`SELECT cemail FROM health_company1.userinfo WHERE cemail NOT IN (SELECT email FROM ssoapp.sso_users where user_type = 'M')`);
     return {unreg:unreg, reg:reg, unreg_counts:unreg.length, reg_counts:reg.length};
   } catch (err) {
     console.error(err);
